d24c5e376647405936c0a03378788e5adc23df6c
Temon137 <ak973@yandex.ru>
2018-11-03 16:16:36 +0400

    Фикс форматирования и текст сообщений.

--- RolForServer/Controllers/HomeController.cs
+++ RolForServer/Controllers/HomeController.cs
@@ -15,9 +15,9 @@ namespace RolForServer.Controllers {
         public IActionResult Forum() {
             ViewBag.Messages = new Messages {
                 Topic = new List<Message> {
-                    new Message {Text = "1"},
-                    new Message {Text = "2"},
-                    new Message {Text = "3"},
+                    new Message {Text = "Первое сообщение."},
+                    new Message {Text = "Второе сообщение, текста стало больше, строка имеет достаточно большую длину. Теперь работают переносы строк.<br>1<br>2<br>3<br>4<br>5<br>6<br>7<br>8<br>9<br>10<br>11<br>12<br>13<br>14"},
+                    new Message {Text = "Третье. Ещё текст."},
                     new Message {Text = "4"}
                 }
             };
