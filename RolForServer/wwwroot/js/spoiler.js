128440946883febd8451f192106494ff13e204dc
Temon137 <ak973@yandex.ru>
2018-11-10 19:08:51 +0400

    Добавлена обёртка в анонимную функцию.

--- RolForServer/wwwroot/js/spoiler.js
+++ RolForServer/wwwroot/js/spoiler.js
@@ -1,12 +1,14 @@
-const texts = document.getElementsByClassName("spoiler-text");
-const buttons = document.getElementsByClassName("spoiler-button");
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', function () {
-        if (texts[i].style.height === '0px' || texts[i].style.height == 0) {
-            texts[i].style.height = texts[i].scrollHeight + 'px';
-        }
-        else {
-            texts[i].style.height = '0px';
-        }
-    });
-}
+(function () {
+    const texts = document.getElementsByClassName("spoiler-text");
+    const buttons = document.getElementsByClassName("spoiler-button");
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function () {
+            if (texts[i].style.height === '0px' || texts[i].style.height == 0) {
+                texts[i].style.height = texts[i].scrollHeight + 'px';
+            }
+            else {
+                texts[i].style.height = '0px';
+            }
+        });
+    }
+})();
