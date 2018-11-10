00cb38c387084472358dfd38d668b23e806c0f9a
Temon137 <ak973@yandex.ru>
2018-11-10 19:08:29 +0400

    Добавлена кнопка прокрутки в начало страницы.

---
+++ RolForServer/wwwroot/js/toTopButton.js
@@ -0,0 +1,25 @@
+(function () {
+    const goTopBtn = document.getElementById('to-top-button');
+
+    window.addEventListener('scroll', trackScroll);
+    goTopBtn.addEventListener('click', backToTop);
+
+    function trackScroll() {
+        const scrolled = window.pageYOffset;
+        const coords = document.documentElement.clientHeight;
+
+        if (scrolled > coords) {
+            goTopBtn.classList.add('to-top-button-show');
+        }
+        if (scrolled < coords) {
+            goTopBtn.classList.remove('to-top-button-show');
+        }
+    }
+
+    function backToTop() {
+        if (window.pageYOffset > 0) {
+            window.scrollBy(0, -30);
+            setTimeout(backToTop, 0);
+        }
+    }
+})();
